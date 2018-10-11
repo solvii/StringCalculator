50e0416eb24ca3fd8ec352bfeda8313c93baabe3
solvii <solviv16@ru.is>
2018-10-11 20:31:04 +0000

    sum of two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,13 @@ function add(numbers){
   if(numbers == "")
   return 0;
 
+  if(numbers.includes(","))
+  {
+    var numberArray = numbers.split(",");
+    return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+  }
+
+  else
   return parseInt(numbers);
 }
 
